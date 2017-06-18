d9c6a74757363e9ea98e8d45bfcb9c9b03437da0
pankajapj23 <pankajapj23@gmail.com>
2017-06-18 22:50:55 +0530

    Insert operation in Sorted Array -Working

--- Array/Array/Array/insertInSortedArray.cpp
+++ Array/Array/Array/insertInSortedArray.cpp
@@ -1,28 +1,31 @@
 #include<stdio.h>
 #include<memory.h>
 
-int arr[20];
 //This function returns the position to insert the new node in sorted array
 int findPosition(int *arr, int low, int high, int key)
 {
     if(key < arr[low])
+		//If element to enter is less than first element then insert the element at first position
 		return low;
 	else if(key > arr[high])
-		return high;
+		//If element to enter is greater than last element then insert the element after last element i.e high+1
+		return high+1;
+	
 	else
 	{
 		int mid = (low+high)/2;
-
 		if(key > arr[mid])
 			return findPosition(arr, mid+1, high, key);
 		else
 			return findPosition(arr, low, mid-1, key);
 	}
+	
 }
 
 int main()
 {
-	 int curSize=5; int arr[20];
+	int curSize=5;
+	int arr[20];
 	memset(arr,0,sizeof(arr)); //initialize array to 0
     
 	//Enter the data in array
@@ -37,25 +40,26 @@ int main()
 	for(int i=0; i<curSize; i++)
 		printf(" %d ", arr[i]);
 
-	int posToAdd = findPosition(arr,0,curSize,25);
-	printf("\nPos to Add %d",posToAdd );
+	int elementToInsert =15;
+
+	//curSize-1 is passed as high index because array start index is 0
+	int posToAdd = findPosition(arr,0,curSize-1,elementToInsert);
 	
 	int i=curSize-1;
 
+	//Push the element to insert the new element to maintain the sorted array
 	while(i>= posToAdd)
 	{
 	  arr[i+1] = arr[i];
 	  i--;
 	}
-
-
-	arr[posToAdd] = 25;
+    //Insert the new element
+	arr[posToAdd] = elementToInsert;
 
 	//Display array
 	printf("\nArray after insertion: ");
 	for(int i=0; i<curSize+1; i++)
 		printf(" %d ", arr[i]);
-
 
 	printf("\n\n");
 	return 0;
